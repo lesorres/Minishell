89e280a0eee09e96964a91071941c2ea0da7c74d
fhyman <ashilmira@gmail.com>
2021-05-19 19:17:49 +0300

    read_line updated, Makefile recreated & libft's Makefile modified

--- read_line.c
+++ read_line.c
@@ -1,8 +1,5 @@
 #include "minishell.h"
 
-// -ltermcap использовать при компиляции в makefile
-// gcc main1.c minishell.h ft_strcmp.c -ltermcap ./libft_ash/ft_putstr_fd.c 
-
 char	*ft_realloc(char *ptr, unsigned int size)
 {
 	char			*new;
@@ -24,13 +21,14 @@ char	*read_line(int fd)
 	char    *buf;
 	int     i;
 	int		num;
+	FILE *file;
 
 	num = 1;
 	buf = (char *)malloc(num);
 	ret = 1;
 	i = 0;
-	while (ret > 0)
-	{
+	// while (ret > 0)
+	// {
 		ret = read(fd, &buf[i], 1);
 		if (buf[i] != '\n' && ret > 0)
 		{
@@ -38,7 +36,7 @@ char	*read_line(int fd)
 			// printf("%c", buf[i]);
 			i++;
 		}
-	}
+	// }
 	buf[i] = '\0';
 	return (buf);
 }
@@ -47,7 +45,7 @@ char	*read_line(int fd)
 // {
 // 	int fd = 0;
 
-// 	// fd = open(argv[1], O_RDONLY);
+// 	// fd = open(argv[1], O_RDONLY);q
 // 	// read_line(fd);
 // 	printf("%s\n", read_line(fd));
 // 	return (0);
