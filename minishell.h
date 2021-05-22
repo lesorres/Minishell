93b98f2205d88382716ebfdfa43f2d3a81f6b12f
fhyman <ashilmira@gmail.com>
2021-05-22 13:43:45 +0300

    updated minishell.h

--- minishell.h
+++ minishell.h
@@ -5,10 +5,11 @@
 # include <unistd.h>
 # include <string.h> 
 # include "libft/libft.h"
-// # include "get_next_line.h"
+# include "get_next_line.h"
 # include "execute_header.h"
 # include <curses.h>
 # include <term.h>
+# include <fcntl.h>
 # include "parser_header.h"
 
 
@@ -19,7 +20,7 @@ typedef struct s_command
 	int		echo;
 	int		cd;
 	int		pwd;
-	int		export;
+	int		exprt;
 	int		unset;
 	int		env;
 	int		exit;
@@ -27,7 +28,9 @@ typedef struct s_command
 
 typedef struct s_all
 {
-	t_cmd		cmd;
+	char	**hist_arr;
+    int     cmd_n;
+    t_cmd   *cmd;
 }				t_all;
 
 int		ft_strcmp(char *s1, char *s2);
@@ -37,6 +40,9 @@ void	parser(char *line, t_all *all);
 
 /*  buildin commands  */
 
-int cmd_echo(int argc, char **argv);
+int 	cmd_echo(t_cmd *cmd, char **argv, char **envp);
+int 	cmd_cd(t_cmd *cmd, char **argv, char **envp);
+void    cmd_env(char **envp);
+void    cmd_pwd(t_cmd *cmd, char **arg, char **envp);
 
 #endif
