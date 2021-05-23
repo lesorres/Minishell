4cf2783a3ff2e2f524cba6cc6575901cc6d7e896
kmeeseek <beaverdi147@gmail.com>
2021-05-23 19:25:41 +0300

    partially working parser if  all->cmd_n more than 3

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 16:04:37 by kmeeseek          #+#    #+#             */
-/*   Updated: 2021/05/23 14:28:38 by kmeeseek         ###   ########.fr       */
+/*   Updated: 2021/05/23 19:22:19 by kmeeseek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,27 +53,77 @@ void	realloc_cmd(t_all *all)
 // 	return(1);
 // }
 
- void 	first_mem_alloc(t_all *all)
- {
+//  void 	first_mem_alloc(t_all *all)
+//  {
+// 	int i;
+
+// 	i = 0;
+// 	all->cmd_n = all->cmd_n + 7;
+// 	// all->cmd[i].arg_n = 7;
+// 	all->cmd = ft_calloc(all->cmd_n, sizeof(t_cmd));
+// 	// if (!all->cmd)
+// 	// 	error();
+// 	// all->cmd[all->cmd_n] = 0;
+// 	while (i < all->cmd_n)
+// 	{
+// 		all->cmd[i].arg_n = 7;
+// 		all->cmd[i].arg = ft_calloc(all->cmd[i].arg_n, sizeof(char*));
+// 		all->cmd[i].redir = ft_calloc(all->cmd[i].arg_n, sizeof(char*));
+// 		i++;
+// 	}
+//  }
+
+void 	cmd_mem_alloc(t_all *all, int i)
+{
+	all->cmd[i].arg_n = all->cmd[i].arg_n + 3;
+	all->cmd[i].arg = ft_calloc(all->cmd[i].arg_n, sizeof(char *)); //не забудь проверку на NULL
+	// all->cmd[i].redir = ft_calloc(all->cmd[i].arg_n, sizeof(char *)); //не забудь проверку на NULL
+}
+
+void 	cmd_arr_mem_alloc(t_all *all)
+{
 	int i;
+	int old_cmd_n;
+	t_cmd *tmp;
 
 	i = 0;
-	all->cmd_n = 7;
-	// all->cmd[i].arg_n = 7;
-	all->cmd = ft_calloc(all->cmd_n, sizeof(t_cmd));
-	// if (!all->cmd)
-	// 	error();
-	// all->cmd[all->cmd_n] = 0;
-	while (i < all->cmd_n)
+	old_cmd_n = all->cmd_n;
+	all->cmd_n = all->cmd_n + 4;
+	if (!all->cmd)
 	{
-		all->cmd[i].arg_n = 7;
-		all->cmd[i].arg = ft_calloc(all->cmd[i].arg_n, sizeof(char*));
-		all->cmd[i].redir = ft_calloc(all->cmd[i].arg_n, sizeof(char*));
-		i++;
+		all->cmd = ft_calloc(all->cmd_n, sizeof(t_cmd)); //не забудь проверку на NULL
+		while (i < all->cmd_n)
+		{
+			all->cmd[i].arg_n = 0;
+			cmd_mem_alloc (all, i);
+			i++;
+		}
 	}
- }
-
-// void new_cmd()
+	else
+	{
+		tmp = ft_calloc(all->cmd_n, sizeof(t_cmd)); //не забудь проверку на NULL
+		while (i < old_cmd_n)
+		{
+			// tmp->name = all->cmd->name;
+			tmp->arg_n = all->cmd->arg_n;
+			tmp->arg = all->cmd->arg;
+			// tmp->redir = all->cmd->redir;
+			// tmp->echo_n = all->cmd->echo_n;
+			// tmp->doub = all->cmd->doub;
+			// tmp->sing = all->cmd->sing;
+			i++;
+		}
+		all->cmd = tmp;
+		free(tmp);
+		// i = 0;
+		// while (old_cmd_n < all->cmd_n)
+		// {
+		// 	all->cmd[old_cmd_n].arg_n = 0;
+		// 	cmd_mem_alloc (all, old_cmd_n);
+		// 	old_cmd_n++;
+		// }
+	}
+}
 
 void	parser(char *line, t_all *all)
 {
@@ -87,13 +137,15 @@ void	parser(char *line, t_all *all)
 	i = 0;
 	j = 0;
 	n = 0;
-
+	all->cmd_n = 0;
 	line = ft_strtrim(line, " ");
 	line_len = ft_strlen(line);
-	// count_max_words(line);
-	first_mem_alloc(all);
- 	while(line[i])
+	// first_mem_alloc(all);
+	cmd_arr_mem_alloc(all);
+	while(line[i])
 	{
+		// if (j % 3 == 0)
+		// 	cmd_arr_mem_alloc(all);
 		k = 0;
 		while (line[i] == ' ')
 			i++;
@@ -116,15 +168,17 @@ void	parser(char *line, t_all *all)
 		}
 		else
 		{
-		all->cmd[j].arg[n] = malloc(ft_strlen(tmp));
-		ft_strcpy(all->cmd[j].arg[n], tmp);
-		n++;
-		all->cmd->arg_n = n;
+			all->cmd[j].arg[n] = malloc(ft_strlen(tmp));
+			ft_strcpy(all->cmd[j].arg[n], tmp);
+			n++;
+			all->cmd->arg_n = n;
 		}
 		free(tmp);
 		// int z = make_line(all->cmd[j].arg[n], line[i]);
 		// all->cmd[j].arg[n][k] = line[i];
 	}
+		// if (all->cmd_n % 3 == 0)
+		// 	cmd_arr_mem_alloc(all);
 	n = 0;
 	j = 0;
 	while (all->cmd[j].arg[n])
--- parser_header.h
+++ parser_header.h
@@ -6,7 +6,7 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 16:13:59 by kmeeseek          #+#    #+#             */
-/*   Updated: 2021/05/21 23:06:17 by kmeeseek         ###   ########.fr       */
+/*   Updated: 2021/05/23 15:10:32 by kmeeseek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@
 
 typedef struct	s_cmd
 {
-	// int		av_arg_num;
+	// char	*name;
 	int		arg_n;
-	char	*name;
 	char	**arg;
-	char	**redir;
-	int		echo_n;
-	int		doub;
-	int		sing;
+	// char	**redir;
+	// int		echo_n;
+	// int		doub;
+	// int		sing;
 }				t_cmd;
 
 #endif
