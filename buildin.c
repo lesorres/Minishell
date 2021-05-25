22c6f7b0676b05aab894a3f209274bbb284ba36a
fhyman <ashilmira@gmail.com>
2021-05-25 22:21:54 +0300

    added buildin cmds env and pwd

--- buildin.c
+++ buildin.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void cmd_echo(t_cmd *cmd, char **argv)
+void cmd_echo(t_all *all, char **argv)
 {
     int     i;
     int     j;
@@ -8,47 +8,58 @@ void cmd_echo(t_cmd *cmd, char **argv)
     i = 1;
     while (argv[i])
     {
-        j = 0;
-        while (argv[i][j])
+        // j = 0;
+        // while (argv[i][j])
         {
-            write(1, argv[i][j++], 1);
-            if (strcmp(argv[1], "-n"))
-                i = 2;    
-            else
-                write(1, "\n", 1);
+            printf("%s\n", argv[i]);
+            // if (strcmp(argv[1], "-n"))
+            //     i = 2;    
+            // else
+            //     write(1, "\n", 1);
             i++;
         }
     }
 }
 
-void cmd_cd(t_cmd *cmd, char **argv, char **envp)
+void cmd_cd(t_all *all, char **argv, char **envp)
 {
     int     dir;
 
     if (argv[1])
     {
         dir = chdir(argv[1]);
-        printf("%s", dir);
+        // printf("%s", dir);
     }
     else if (!argv[1])
         chdir("/");
 }
 
-void    cmd_pwd(t_cmd *cmd, char **arg, char **envp)
+int		ft_putstr(char *str)
 {
-    char *path;
-    char *tmp;
+	int	i;
+	int	ret;
 
-    path = malloc(sizeof(char) * PATH_LEN);
-    if (!path)
-        return (NULL);
-    getcwd(tmp, PATH_LEN);
-    if (ft_strlen(tmp) > PATH_LEN)
-        path = ft_realloc(tmp, PATH_LEN * 2);
-    ft_strcpy(path, tmp);
-    printf("%s/n", path);
-    free (tmp);
-    free (path);
+	i = -1;
+	ret = 0;
+	while (str[++i])
+		ret += write(1, &str[i], 1);
+	return (ret);
+}
+
+void    cmd_pwd(t_all *all, char **arg, char **envp)
+{
+    char    *path;
+    char    *tmp;
+
+    path = malloc(PATH_LEN + 1);
+    // if (!path)
+        // return (-1);
+    tmp = getcwd(path, PATH_LEN);
+    write(1, tmp, ft_strlen(tmp));
+    write(1, "\n", 1);
+    free(path);
+	// return (1);
+    (void)arg;
 }
 
 void    cmd_env(char **envp)
@@ -61,23 +72,42 @@ void    cmd_env(char **envp)
         printf("%s\n", envp[i]);
         i++;
     }
-    return (1);
 }
 
-void    buildin_func(t_cmd *cmd, char **arg, char **envp)
+// void    buildin_func(t_all *all, char **arg, char **envp)
+// {
+//     if (strcmp(arg[0], "cd "))
+//         cmd_cd(all, arg, envp);
+//     else if (strcmp(arg[0], "echo "))
+//         cmd_echo(all, arg);
+//     else if (strcmp(arg[0], "pwd"))
+//         cmd_pwd(all, arg, envp);
+//     else if (strcmp(arg[0], "export"))
+//         cmd_export(all, arg, envp);
+//     else if (strcmp(arg[0], "unset "))
+//         cmd_unset(all, arg, envp);
+//     else if (strcmp(arg[0], "env"))
+//         cmd_env(envp);
+//     else if (strcmp(arg[0], "exit"))
+//         cmd_exit(arg);
+// }
+
+void    buildin_func(t_all *all, char **arg, char **envp)
 {
-    if (strcmp(arg[0], "cd "))
-        cmd_cd(cmd, arg, envp);
-    else if (strcmp(arg[0], "echo "))
-        cmd_echo(cmd, arg);
-    else if (strcmp(arg[0], "pwd"))
-        cmd_pwd(cmd, arg, envp);
-    else if (strcmp(arg[0], "export"))
-        cmd_export(cmd, arg, envp);
-    else if (strcmp(arg[0], "unset "))
-        cmd_unset(cmd, arg, envp);
-    else if (strcmp(arg[0], "env"))
+    if (!strcmp(all->cmd[0].arg[0], "cd"))
+        cmd_cd(all, arg, envp);
+    else if (!strcmp(all->cmd[0].arg[0], "echo"))
+        cmd_echo(all, arg);
+    // printf("%s\n", all->cmd->arg[0]);
+    // write(1, all->cmd[0].arg[0], ft_strlen(arg[0]));
+    if (!strcmp(all->cmd[0].arg[0], "pwd"))
+        cmd_pwd(all, arg, envp);
+    // else if (strcmp(all->cmd->arg[0], "export"))
+    //     cmd_export(all, arg, envp);
+    // else if (strcmp(all->cmd->arg[0], "unset "))
+    //     cmd_unset(all, arg, envp);
+    else if (!strcmp(all->cmd[0].arg[0], "env"))
         cmd_env(envp);
-    else if (strcmp(arg[0], "exit"))
-        cmd_exit(arg);
+    // else if (strcmp(all->cmd->arg[0], "exit"))
+    //     cmd_exit(arg);
 }
