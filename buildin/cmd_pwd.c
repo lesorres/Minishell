7ebe4da4e76a555664b71822f33756729d7decd1
fhyman <ashilmira@gmail.com>
2021-06-01 21:10:07 +0300

    created new function for pwd

---
+++ buildin/cmd_pwd.c
@@ -0,0 +1,13 @@
+#include "../minishell.h"
+
+void    cmd_pwd(t_all *all, char **envp)    // ok???
+{
+	char    *path;
+	char    *tmp;
+
+	path = malloc(PATH_LEN + 1);
+	tmp = getcwd(path, PATH_LEN);
+	write(1, tmp, ft_strlen(tmp));
+	write(1, "\n", 1);
+	free(path);
+}
