30fd42fc7b1d91fa83545e66e3b03c843bd31fe5
fhyman <ashilmira@gmail.com>
2021-06-03 22:18:29 +0300

    export add new args into arr but its still work wrong

--- buildin/cmd_export.c
+++ buildin/cmd_export.c
@@ -72,16 +72,49 @@ int		ft_strchr_int(const char *str, int c)
 		return (0);
 }
 
-void	check_val(t_all *all, char *line)
+void	check_env(t_all *all, char *line, int k)
+{
+	int 	i;
+	int		j;
+	int		dif;
+	char	*env_tmp;
+
+	i = 0;
+	printf("\n\n tmp here -------= |%s|\n", all->tline.replaced_str);
+	while (all->tline.env_arr[i])
+	{
+		printf("\n env_arr ------ |%s|\n", all->tline.env_arr[i]);
+		j = ft_strchr_int(all->tline.env_arr[i], '=');
+		env_tmp = ft_substr(all->tline.env_arr[i], 0, j);
+		printf("\nenv_tmp ------ |%s|\n", env_tmp);
+		if (ft_strcmp(env_tmp, all->tline.replaced_str) != 0)
+		{
+			all->tline.env_arr[i] = ft_realloc(all->tline.env_arr[i], ft_strlen(line));
+			ft_strcpy(all->tline.env_arr[i], line);
+		}
+		else
+			i++;
+	}
+}
+
+void	check_val(t_all *all, char *line, int k)
 {
 	char	*tmp;
 	int		i;
 	int		j;
+	int		len;
 
 	i = 0;
 	j = ft_strchr_int(line, '=');
 	tmp = ft_substr(line, 0, j);
-	printf("\n\ntmp = |%s|\n", tmp);
+	len  = ft_strlen(line) - j;
+	all->tline.replaced_val = malloc(sizeof(len));
+	all->tline.replaced_val = ft_substr(line, j, len);
+	all->tline.replaced_str = malloc(sizeof(ft_strlen(tmp)));
+	all->tline.replaced_str = tmp;
+	printf("\n\ntmp = |%s|\n", all->tline.replaced_str);
+	printf("\n\nthis isn't tmp = |%s|\n", all->tline.replaced_val);
+	check_env(all, line, k);
 }
 
 void    cmd_export(t_all *all, int k)
@@ -93,16 +126,19 @@ void    cmd_export(t_all *all, int k)
 	arr_len = len(all->tline.export_arr);
 	if (all->cmd[k].arg[0])
 	{
-		check_val(all, all->cmd[k].arg[0]);
+		check_val(all, all->cmd[k].arg[0], k);
         // проверять есть ли такая переменная окружения в массиве, если да - заменять значение
 		add_new_env_param(all, all->cmd[k].arg[0]);
 	}
 	arr_len = len(all->tline.export_arr);
 	copy_env(all);
 	sort_env(all);
-	while (all->tline.export_arr[i])
+	if (!all->cmd[k].arg[0])
 	{
-	    printf("declare -x %s\n", all->tline.export_arr[i]);
-		i++;
+		while (all->tline.export_arr[i])
+		{
+		    printf("declare -x %s\n", all->tline.export_arr[i]);
+			i++;
+		}
 	}
 }
