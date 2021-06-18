854c3c5b338af0f7409d7f6e4b6b95d8ae8058f9
fhyman <ashilmira@gmail.com>
2021-06-18 21:10:27 +0300

    cmd_exit: value of status added

--- buildin/cmd_exit.c
+++ buildin/cmd_exit.c
@@ -5,13 +5,15 @@ int		exit_error(t_all *all, char *cmd, char *arg, int k)
 	printf("%s\n", all->cmd[k].arg[0]);
 	write(1, "minishell: ", PROMPT);
 	printf("%s: %s: %s\n", cmd, arg, "numeric argument required");
-	return (0);
+	all->status = "255";
+	exit (0);
+	// return (0);
 }
 
 void	print_exit(t_all *all, int k)
 {
 	printf("%s\n", all->cmd[k].arg[0]);
-	exit (0);
+	// exit (0);
 }
 
 int	isdigit_line(char *str)
@@ -31,25 +33,43 @@ int	isdigit_line(char *str)
 
 void	cmd_exit(t_all *all, char **arg, int k)
 {
+	char	*status;
+	int		stts;
+
 	if (all->cmd[k].arg[1])
 	{
 		if (isdigit_line(all->cmd[k].arg[1]))
 		{
 			if (!all->cmd[k].arg[2])
+			{
 				print_exit(all, k);
+				stts = ft_atoi(all->cmd[k].arg[1]);
+				stts %= 255;
+				printf("stts - atoi = %d\n", stts);
+				status = ft_itoa(stts);
+				printf("status - itoa = %s\n", all->status);
+				// status = " ";
+				exit (0);
+			}
 			else
 			{
                 printf("%s\n", all->cmd[k].arg[0]);
 				write(1, "minishell: ", PROMPT);
 				printf("%s: %s \n", all->cmd[k].arg[0], "too many arguments");
+				status = "1";
 			}
 		}
 		else
 		{
 			exit_error(all, all->cmd[k].arg[0], all->cmd[k].arg[1], k);
-			exit (0);
+			// all->status = "255";
+			// exit (0);
 		}
 	}
 	else
+	{
 		print_exit(all, k);
+		all->status = "0";
+		exit (0);
+	}
 }
