2e854d0bcd8e4c652041cbfe913a1dee7483ad3c
fhyman <ashilmira@gmail.com>
2021-06-29 20:56:37 +0300

    cmd_echo has status

--- buildin/cmd_echo.c
+++ buildin/cmd_echo.c
@@ -3,23 +3,24 @@
 void cmd_echo(t_all *all, char **argv, int k)
 {
 	int     i;
-	int		f;
-	
-	f = 0;
+
 	i = 1;
 	if (all->cmd[k].arg[i] == NULL && all->cmd[k].echo_n != 1)
 	{
 		write(1, "\n", 1);
 		return ;
 	}
-	while (!all->cmd[k].null && all->cmd[k].arg[i])          // аргументы echo в парсере должны обрабатываться как один
+	else
 	{
-		write(1, all->cmd[k].arg[i], ft_strlen(all->cmd[k].arg[i]));
-		if (!(all->cmd[k].arg[i + 1] == 0))
-			write(1, " ", 1);
-		i++;
+		while (!all->cmd[k].null && all->cmd[k].arg[i])
+		{
+			write(1, all->cmd[k].arg[i], ft_strlen(all->cmd[k].arg[i]));
+			if (!(all->cmd[k].arg[i + 1] == 0))
+				write(1, " ", 1);
+			i++;
+		}
 	}
 	if (all->cmd[k].echo_n != 1)
 		write(1, "\n", 1);
-	status = "0";
+	status = 0;
 }
