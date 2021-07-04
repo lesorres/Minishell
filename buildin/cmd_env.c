f0f507a3898ab5511a621b99fc4085030a16a64b
fhyman <ashilmira@gmail.com>
2021-07-04 15:39:42 +0300

    env fitted to norms

--- buildin/cmd_env.c
+++ buildin/cmd_env.c
@@ -1,32 +1,36 @@
 #include "../minishell.h"
 
-void    get_envp(t_all *all, char **envp)
+void	get_envp(t_all *all, char **envp)
 {
-	int     line;
-	
+	int	line;
+
 	all->tline.env_line_num = 0;
 	line = 0;
 	while (envp[all->tline.env_line_num])
 		all->tline.env_line_num++;
-	all->tline.env_arr = (char **)malloc(sizeof(char *) * (all->tline.env_line_num + 1));
+	// all->tline.env_arr = (char **)malloc(sizeof(char *)
+		// * (all->tline.env_line_num + 1));
+	all->tline.env_arr = ft_calloc((all->tline.env_line_num + 1),
+			sizeof(char *));
 	while (line < all->tline.env_line_num)
 	{
 		all->tline.env_arr[line] = ft_strdup(envp[line]);
 		line++;
 	}
-	all->tline.env_arr[line] = NULL;
+	// all->tline.env_arr[line] = NULL;
 	check_shlvl(all, all->tline.env_arr);
 	split_path(all);
 }
 
-void    cmd_env(t_all *all, int k)
+void	cmd_env(t_all *all, int k)
 {
-	int     i;
+	int	i;
 
 	i = 0;
 	if (all->cmd[k].arg[1])
 	{
-		printf("%s: %s: %s\n", all->cmd[k].arg[0], all->cmd[k].arg[1], "No such file or directory");
+		printf("%s: %s: %s\n", all->cmd[k].arg[0], all->cmd[k].arg[1],
+			"No such file or directory");
 		status = 127;
 	}
 	else
