645fd95d46326637d6279ee47d491e544b5024c2
fhyman <ashilmira@gmail.com>
2021-07-04 15:37:33 +0300

    cmd_cd is split into 2 file and now second part in file cd_utils

---
+++ buildin/cd_utils.c
@@ -0,0 +1,46 @@
+#include "../minishell.h"
+
+int	error_handler(char *arg1, char *arg2, char *err)
+{
+	write(2, "minishell: ", PROMPT);
+	write(2, arg1, ft_strlen(arg1));
+	write(2, ": ", 2);
+	write(2, arg2, ft_strlen(arg2));
+	write(2, ": ", 2);
+	write(2, err, ft_strlen(err));
+	write(2, "\n", 1);
+	return (1);
+}
+
+void	add_oldpwd(t_all *all, char *path)
+{
+	int		i;
+
+	i = 0;
+	while (all->tline.env_arr[i])
+	{
+		if (ft_strncmp(all->tline.env_arr[i], "OLDPWD=", 7) == 0)
+		{
+			all->builds.oldpwd = ft_strjoin("OLDPWD=", path);
+			all->tline.env_arr[i] = ft_realloc(all->tline.env_arr[i],
+					ft_strlen(all->builds.oldpwd));
+			ft_strcpy(all->tline.env_arr[i], all->builds.oldpwd);
+			break ;
+		}
+		i++;
+	}
+	if (all->tline.env_arr[i] == NULL && all->builds.oldpwd == NULL)
+	{
+		all->builds.oldpwd = ft_strjoin("OLDPWD=", path);
+		add_new_env_param(all, all->builds.oldpwd);
+	}
+}
+
+char	*add_path(t_all *all, char **envp)
+{
+	char	*path;
+
+	path = malloc(PATH_LEN + 1);
+	path = getcwd(path, PATH_LEN);
+	return (path);
+}
