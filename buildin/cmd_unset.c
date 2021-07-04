5e1262c20fd7b21c8d273cf82e94b01e8319163a
fhyman <ashilmira@gmail.com>
2021-07-04 15:42:41 +0300

    unset fitted to norms

--- buildin/cmd_unset.c
+++ buildin/cmd_unset.c
@@ -1,9 +1,9 @@
 #include "../minishell.h"
 
-void    del_env_line(t_all *all, int i)
+void	del_env_line(t_all *all, int i)
 {
-    char    **new_arr;
-    int     j;
+	char	**new_arr;
+	int		j;
 
 	j = 0;
 	free(all->tline.env_arr[i]);
@@ -28,39 +28,48 @@ void	free_path_arr(t_all *all)
 	free (all->path_arr);
 }
 
-int    cmd_unset(t_all *all, int k)
+void	check_cycle(t_all *all, int k)
 {
-    int 	i;
+	int		i;
 	int		j;
 	int		n;
 	char	*tmp;
 
 	n = 1;
-    if (!all->cmd[k].arg[1])
-        return (0);
-    else
-    {
+	while (all->cmd[k].arg[n])
+	{
+		i = 0;
+		if (check_valid_id(all->cmd[k].arg[0], all->cmd[k].arg[n]))
+			return ;
+		else
+		{
+			while (all->tline.env_arr[i])
+			{
+				j = find_env_equal(all->tline.env_arr[i]);
+				tmp = ft_substr(all->tline.env_arr[i], 0, j);
+				if (ft_strcmp(tmp, all->cmd[k].arg[n]) == 0)
+					del_env_line(all, i);
+				i++;
+				free (tmp);
+			}
+			n++;
+		}
+	}
+}
+
+int	cmd_unset(t_all *all, int k)
+{
+	int		i;
+	int		n;
+
+	n = 1;
+	if (!all->cmd[k].arg[1])
+		return (0);
+	else
+	{
 		if (!ft_strncmp(all->cmd[k].arg[n], "PATH", 4))
 			free_path_arr(all);
-        while (all->cmd[k].arg[n])
-        {
-    		i = 0;
-			if (check_valid_id(all->cmd[k].arg[0], all->cmd[k].arg[n]))
-				return (1);
-			else
-			{
-				while (all->tline.env_arr[i])
-				{
-					j = find_env_equal(all->tline.env_arr[i]);
-					tmp = ft_substr(all->tline.env_arr[i], 0, j);
-					if (ft_strcmp(tmp, all->cmd[k].arg[n]) == 0)
-            			del_env_line(all, i);
-					i++;
-					free (tmp);
-				}
-        		n++;
-			}
-        }
-    }
-    return(0);
+		check_cycle(all, k);
+	}
+	return (0);
 }
