7bea99be2793e57c5309be0de6577ee9d31be788
fhyman <ashilmira@gmail.com>
2021-07-04 22:05:45 +0300

    kill_sig function deleted

--- signals.c
+++ signals.c
@@ -1,26 +1,18 @@
 #include "minishell.h"
 
-void    int_sign(int sign)
+void	int_sign(int sign)
 {
-    if (sign == SIGINT)
-    {
-        write(1, "\n", 1);
-        // write(1, "#minishell> ", PROMPT);
-        signal(SIGINT, int_sign);
-    }
+	status = 130;
+	if (sign == SIGINT)
+	{
+		write(1, "\n", 1);
+		signal(SIGINT, int_sign);
+	}
 }
 
-void    quit_sign(int sign)
+void	quit_sign(int sign)
 {
-    if (sign == SIGQUIT)
-        write(1, "Quit: 3\n", 9);
-}
-
-void    kill_sig(int sign)
-{
-    if (sign == EOF)
-    {
-        write(1, "exit\n", 5);
-        exit (1);
-    }
+	status = 131;
+	if (sign == SIGQUIT)
+		write(1, "\n", 1);
 }
