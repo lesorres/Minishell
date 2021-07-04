a1e9aa99d3c9834888aff1811d8bdaa58c9f35c6
fhyman <ashilmira@gmail.com>
2021-07-04 15:19:40 +0300

    function added to header

--- minishell.h
+++ minishell.h
@@ -12,7 +12,6 @@
 # include <term.h>
 # include <fcntl.h>
 # include <signal.h>
-// # include "parser_header.h"
 # include <errno.h>
 # include <sys/param.h>
 # include <sys/types.h>
@@ -24,7 +23,7 @@ typedef struct s_lst
 	char	*oldpwd;
 	int		export_new_arg;
 	int		get_env;
-}   t_lst;
+}	t_lst;
 
 typedef struct	s_cmd
 {
@@ -41,8 +40,8 @@ typedef struct	s_cmd
 
 typedef struct s_all
 {
-    int			cmd_n;
-    t_cmd		*cmd;
+	int			cmd_n;
+	t_cmd		*cmd;
 	t_lst		builds;
 	pid_t		*pid;
 	t_termline	tline;
@@ -55,7 +54,7 @@ typedef struct s_all
 	int			cmd_i;
 	int			in;
 	int			out;
-	int			set; 			//зашла ли функция во второй цикл и нужно ли создавать аргумент по пустому tmp
+	int			set;			//зашла ли функция во второй цикл и нужно ли создавать аргумент по пустому tmp
 	char		*tmp;
 }				t_all;
 
@@ -63,16 +62,21 @@ int		status;
 
 int		ft_putchar(char c);
 char	*read_line(int fd);
-int 	len(char **str);
+int		len(char **str);
 char	*add_quotes(t_all *all, char *line);
 char	*add_path(t_all *all, char **envp);
+void	add_oldpwd(t_all *all, char *path);
 int		find_env_equal(char *line);
 void	check_shlvl(t_all *all, char **envp);
-void    int_sign(int sign);
-void    quit_sign(int sign);
-void    kill_sig(int sign);
+void	int_sign(int sign);
+void	quit_sign(int sign);
+void	kill_sig(int sign);
 int		check_valid_id(char *cmd, char *line);
-void	print_err2(t_all *all, char *cmd, char *err_name);
+int		find_equal_sign(t_all *all, char *line);
+void	copy_env(t_all *all);
+void	sort_env(t_all *all);
+void	print_err2(char *cmd, char *err_name);
+int		error_handler(char *arg1, char *arg2, char *err);
 
 /*  buildin commands  */
 
@@ -81,13 +85,15 @@ void	cmd_cd(t_all *all, char **envp, int k);
 void	cmd_echo(t_all *all, char **argv, int k);
 void	cmd_env(t_all *all, int k);
 void	cmd_export(t_all *all, int k);
+
 void	cmd_pwd(t_all *all, char **envp);
 int		cmd_unset(t_all *all, int k);
 void	cmd_exit(t_all *all, char **arg, int k);
+int		cmp_path(t_all *all, char *str, char *name);
 void	add_new_env_param(t_all *all, char *line);
 void	get_envp(t_all *all, char **envp);
 void	split_path(t_all *all);
-void    pipe_exec(t_all *all, char**argv, char **envp);
+void	pipe_exec(t_all *all, char**argv, char **envp);
 
 /*  parser functions  */
 
