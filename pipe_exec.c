9f83a1ab29c5f8d3bb8da5fd3768311318c8ed06
fhyman <ashilmira@gmail.com>
2021-07-09 22:20:44 +0300

    pipe_exec.c: function splited into 2 functions and now complies with norm

--- pipe_exec.c
+++ pipe_exec.c
@@ -1,66 +1,58 @@
 #include  "minishell.h"
 
-// void free_int_2d_arr(int ***arr)
-// {
-// 	int	i;
-	
-// 	i = 0;
-// 	while ((*arr)[i])
-// 		free((*arr)[i++]);
-// 	free(*arr);
-// }
+void	free_int_arr(int **arr)
+{
+	int	i;
 
-void __free_int_arr(int **arr) {
-	int i = 0;
-	while (arr[i]) {
+	i = 0;
+	while (arr[i])
 		free(arr[i++]);
-	}
 	free(arr);
+}
+
+void	pipe_cycle(t_all *all, char**argv, char **envp, int i)
+{
+	pid_t	pid;
+
+	if (i < all->p_num)
+		pipe(all->fd[i]);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (i < all->p_num)
+			dup2(all->fd[i][1], 1);
+		if (i != 0)
+			dup2(all->fd[i - 1][0], 0);
+		buildin_func(all, argv, envp);
+		exit(0);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (i != all->p_num)
+			close(all->fd[i][1]);
+		if (i != 0)
+			close(all->fd[i - 1][0]);
+	}
 }
 
 void	pipe_exec(t_all *all, char**argv, char **envp)
 {
-	int		**fd;
-	pid_t	pid;
 	char	c;
-	int		status;
 	int		k;
 	int		i;
 
 	k = all->p_num;
 	i = 0;
-	// fd = (int**)malloc(sizeof(int*) * (k));
-	fd = ft_calloc((k + 1), sizeof(int *));
-	while (i < k){
-		fd[i] = malloc(sizeof(int) * 2);
-		i++;
-	}
+	all->fd = ft_calloc((k + 1), sizeof(int *));
+	while (i < k)
+		all->fd[i++] = malloc(sizeof(int) * 2);
 	i = 0;
 	while (i < k + 1)
 	{
-		if (i < k)
-			pipe(fd[i]);
-		pid = fork();
-		if (pid == 0)
-		{
-			if (i < k)
-				dup2(fd[i][1], 1);
-			if (i != 0)
-				dup2(fd[i - 1][0], 0);
-			buildin_func(all, argv, envp);
-			// free_int_2d_arr(&fd);
-			exit(0);
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-			if (i != k)
-				close(fd[i][1]);
-			if (i != 0)
-				close(fd[i - 1][0]);
-		}
+		pipe_cycle(all, argv, envp, i);
 		all->cmd_i++;
 		i++;
 	}
-	__free_int_arr(fd);  //вызывает двойное высвобождение памяти, но если убрать, появляются лики
+	free_int_arr(all->fd);
 }
