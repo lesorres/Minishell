94ac896b785a45453952a4bcc214ba5abaed95a0
fhyman <ashilmira@gmail.com>
2021-07-10 21:41:53 +0300

    execute_header: added new var and deleted unused vars

--- execute_header.h
+++ execute_header.h
@@ -1,25 +1,36 @@
-#include "minishell.h"
+#ifndef EXECUTE_HEADER_H
+# define EXECUTE_HEADER_H
 
+# include "minishell.h"
 # define UP "\e[A"
 # define DWN "\e[B"
-# define LEFT "\e[D"
-# define RiGHT "\e[C"
+# define K_LEFT "\e[D"
+# define K_RIGHT "\e[C"
 # define TERM_NAME "xterm-256color"
 # define PROMPT 12
 # define PATH_LEN 1024
 # define CMD_N_F "command not found"
 # define EXP_NOT_VAL "not a valid identifier"
 
+typedef struct s_lst
+{
+	char	*oldpwd;
+	int		export_new_arg;
+	int		get_env;
+	char	*str;
+	int		fd;
+	int		l;
+}	t_lst;
+
 typedef struct s_termline
 {
-	int     cursor;
-	int     symb_num;
-	int     line_num;
+	int		cursor;
+	int		symb_num;
+	int		line_num;
 	int		curr_line;
 	int		env_line_num;
 	char	*print_line;
-	char    *hist_line;
-	char    *str;
+	char	*hist_line;
 	char	**env_arr;
 	int		exp_line_num;
 	char	**export_arr;
@@ -28,9 +39,12 @@ typedef struct s_termline
 	char	*val_str;
 	int		equal_sign;
 	int		first_shlvl;
-	char    *path;
+	char	*path;
 	char	*pwd;
 	char	*new_name;
-}   t_termline;
+	int		unset_path;
+}	t_termline;
 
-int	isdigit_line(char *str);
+int		isdigit_line(char *str);
+
+#endif
